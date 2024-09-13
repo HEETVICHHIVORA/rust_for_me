088cb7cb2af349351167e59c7554dc528d90332d
heetcodes <heetvic@gmail.com>
2024-09-13 14:42:08 +0530

    m

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
 fn main() {
-    println!("{}",fib(5));
+   // println!("{}",fib(5));
+  // let name = String::from ("harkirat");
+  // print!("{}",get_length(name));
+  let rect1 = Rect{
+    width:10,
+    height:20
+  };
+  println!("the area ->>{}",rect1.area());
+  print!("the perimeter-->{}",rect1.perimeter());
 }
-fn fib (num:i32)->i32 {
-    let mut first = 0 ;
-    let mut second  = 1 ;
-    if num==0 {
-        return first ;
-
-    }
-    if num==1{
-       return 1 ;
-    }
-    for _ in 0..(num-1){
-        let temp = second ;
-        second = second + first ;
-        first  = temp ;
-    }
-
-    return  second ;
-
+struct  Rect {
+    width:i32,
+    height:i32,
 }
+impl Rect {
+    fn area(&self)->i32{
+        return (self.width*self.height);
+    }
+    fn perimeter (&self)->i32{
+        return 2*(self.width+self.height); 
+    }
+    
+}
+
+// fn fib (num:i32)->i32 {
+//     let mut first = 0 ;
+//     let mut second  = 1 ;
+//     if num==0 {
+//         return first ;
+
+//     }
+//     if num==1{
+//        return 1 ;
+//     }
+//     for _ in 0..(num-1){
+//         let temp = second ;
+//         second = second + first ;
+//         first  = temp ;
+//     }
+
+//     return  second ;
+
+// }
+// fn get_length (str:String)->usize{
+//     str.chars().count()
+// }
